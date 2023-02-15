e5b3ab7b3abac95bb58b92cce40b63ae040e0131
Toha <you@example.com>
2023-02-15 13:40:19 +0300

    add def to_uppercase_firsr

--- str_func.py
+++ str_func.py
@@ -2,3 +2,6 @@ def to_uppercase(s):
     """Возвращает строку с верхним регистром"""
     return str(s).upper()
 
+def to_uppercase_first(s):
+    """Возвращает строку с верхним регистром первой буквы в слове"""
+    return str(s).title()
